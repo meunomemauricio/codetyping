0cf3b2725ec9d6d3895b53380c2d625ef0d8ac25
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2017-01-29 03:08:16 -0200

    Added Webpack to Karma
    Also had to use istambul-instrumenter-loader to continue using the
    coverage reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,49 @@
-// Karma configuration
-// Generated on Thu Jan 26 2017 18:41:28 GMT-0200 (BRST)
+var path = require('path'),
+    configSettings,
+    TEST_RESULTS_DIR = 'test-results',
+    COVERAGE_DIR =  path.join(TEST_RESULTS_DIR, 'coverage');
 
 module.exports = function(config) {
-  config.set({
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        reporters: ['progress', 'coverage'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['Chrome'],
+        singleRun: true,
+        concurrency: Infinity,
 
+        files: [
+            'test/unit/index.js',
+        ],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: [
-        'jasmine'
-    ],
+        // exclude: [
+        //     'src/codetype.js',
+        // ],
 
+        preprocessors: {
+            'src/*.js': ['coverage'],
+            './test/unit/index.js': ['babel', 'webpack'],
+        },
 
-    // list of files / patterns to load in the browser
-    files: [
-        'src/*.js',
-        'test/unit/*Spec.js',
-    ],
+        webpack: {
+             module: {
+                preLoaders: [
+                    {
+                        test: /\.js$/,
+                        include: path.resolve('src/'),
+                        loader: 'istanbul-instrumenter',
+                    },
+                ]
+            },
+        },
 
-
-    // list of files to exclude
-    exclude: [
-        'src/codetype.js',
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/*.js': 'coverage'
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+        webpackMiddleware: {
+            noInfo: true
+        },
+    })
 }
--- test/unit/wpmCalcSpec.js
+++ test/unit/wpmCalcSpec.js
@@ -1,29 +1,31 @@
+var wpmCalc = require('../../src/wpmCalc.js');
+
 describe("Raw WPM Calculation", function() {
     it("should return the correct value", function() {
-        var value = getRawWPM(60 * 5, 60 * 1000);
+        var value = wpmCalc.getRawWPM(60 * 5, 60 * 1000);
         expect(value).toEqual(60);
     });
 
     it("should return zero when time is zero too", function() {
-        var value = getRawWPM(0, 0);
+        var value = wpmCalc.getRawWPM(0, 0);
         expect(value).toEqual(0);
     });
 });
 
-describe("Net WPM Calculation", function() {
 
+describe("Net WPM Calculation", function() {
     it("should return the correct value", function() {
-        var value = getNetWPM(60 * 5, 5, 60 * 1000);
+        var value = wpmCalc.getNetWPM(60 * 5, 5, 60 * 1000);
         expect(value).toEqual(55);
     });
 
     it("should return zero when time is zero too", function() {
-        var value = getNetWPM(0, 0, 0);
+        var value = wpmCalc.getNetWPM(0, 0, 0);
         expect(value).toEqual(0);
     });
 
     it("should not return negative numbers", function() {
-        var value = getNetWPM(60 * 5, 61, 60 * 1000);
+        var value = wpmCalc.getNetWPM(60 * 5, 61, 60 * 1000);
         expect(value).toEqual(0);
     });
 
